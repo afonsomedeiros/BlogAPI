0186c8bc9419c4f0c44349d64cea592a00336ce3
afonsomedeiros <afonso.b.medeiros@gmail.com>
2021-04-05 20:19:10 -0300

    Atualização lib de gerenciado to Token JWT.

--- app/extension/jwt.py
+++ app/extension/jwt.py
@@ -5,5 +5,13 @@ from modules.authenticate import Auth
 
 
 def install_user(app: Bottle):
-    jwt = JWTPlugin(JWT_SECRET, Auth)
+    configs = [
+        {
+            'model': Auth,
+            'endpoint': '/auth',
+            'auth_name': "auth",
+            'refresh_name': 'refresh'
+        }
+    ]
+    jwt = JWTPlugin(JWT_SECRET, configs, payload=['id', 'status'])
     app.install(jwt)
